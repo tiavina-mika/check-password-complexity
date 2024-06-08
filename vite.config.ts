01d3d661a38f1bef6d2cf47cfa0e63cb3fe34c1e
tiavina-mika <tiavinamika@gmail.com>
2024-06-08 12:30:29 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { UserConfig, defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+} satisfies UserConfig)
