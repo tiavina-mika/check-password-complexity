88ffbb79daaa71e8387cbf42428c9856aba03055
tiavina-mika <tiavinamika@gmail.com>
2024-06-20 21:33:16 +0300

    Create App.tsx

---
+++ demo/src/App.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useState } from "react";
+
+import { checkPasswordComplexity } from "check-password-complexity";
+
+const App = () => {
+  const [password, setPassword] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }
+  
+  return (
+    <div className="root">
+      <div>
+        <h1>Password Complexity Checker</h1>
+      </div>
+      <div className="card">
+        <div className="form">
+          <label>Password</label>
+          <input
+            type="text"
+            value={password}
+            onChange={handleChange}
+            placeholder="Enter the password to check its complexity"
+          />
+        </div>
+
+        <div className="result">
+          <label>Result:</label>
+          <pre>{JSON.stringify(checkPasswordComplexity(password), null, 2)}</pre>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
